66587694ce583b86d257996e2111164c6c679d31
Azamah Jr <depixellife@gmail.com>
2022-12-27 02:11:00 +0100

    Initial Commit

---
+++ Two Fold/Two_FoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Two_FoldApp.swift
+//  Two Fold
+//
+//  Created by Azamah Junior Khan on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Two_FoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
